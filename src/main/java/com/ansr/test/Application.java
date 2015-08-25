7ba910610807a14a08e689cb7fb9e00c8da0918e
lstefan <livia.stefan@endava.com>
2015-08-25 19:07:47 +0300

    Added basic authentication with spring security. User: user. Password: password and login page

--- src/main/java/com/ansr/test/Application.java
+++ src/main/java/com/ansr/test/Application.java
@@ -1,14 +1,40 @@
 package com.ansr.test;
 
-import com.ansr.dto.Address;
-import com.ansr.dto.User;
-import com.ansr.repository.UserRepository;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import com.ansr.repository.UserRepository;
 
 /**
  * Created by astoica on 8/7/2015.
@@ -24,6 +50,7 @@ public class Application implements CommandLineRunner{
     @Autowired
     private UserService userService;
 
+    
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
     }
@@ -35,4 +62,5 @@ public class Application implements CommandLineRunner{
 
         while(true);
     }
+    
 }
