a3cc406148fc20274aecfccbde9581e519a79700
lstefan <livia.stefan@endava.com>
2015-09-14 13:02:01 +0300

    Extracted mongodb configuration details in the properties file

--- src/main/java/com/ansr/config/MongoConfiguration.java
+++ src/main/java/com/ansr/config/MongoConfiguration.java
@@ -1,5 +1,8 @@
 package com.ansr.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -7,9 +10,26 @@ import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 public class MongoConfiguration extends AbstractMongoConfiguration {
+
+	@Value("${databaseName}")
+	private String databaseName;
+
+	@Value("${server}")
+	private String server;
+
+	@Value("${port}")
+	private String port;
+
+	@Value("${userName}")
+	private String userName;
+
+	@Value("${password}")
+	private String password;
 
 	@Bean
 	public GridFsTemplate gridFsTemplate() throws Exception {
@@ -18,13 +38,25 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
 
 	@Override
 	protected String getDatabaseName() {
-		return "test";
+		return databaseName;
 	}
 
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		return new MongoClient("127.0.0.1");
+		ServerAddress serverAddress = new ServerAddress(server, Integer.parseInt(port));
+		MongoCredential credentials = MongoCredential.createMongoCRCredential(userName, databaseName,
+				password.toCharArray());
+		System.out.println("Username " + userName);
+		MongoClient mongoClient;
+		if (!userName.equals("") && !password.equals("")) {
+			mongoClient = new MongoClient(serverAddress, Arrays.asList(credentials));
+		} else {
+			mongoClient = new MongoClient(serverAddress);
+		}
+
+		return mongoClient;
+
 	}
 
 }
